f1114cff7846bee72fe4164ac975bf3d2c9e6ff6
Thiago <159097063+thiago-ultimo@users.noreply.github.com>
2024-06-05 07:00:59 -0300

    teste

--- projeto faculdade Rogério/teste/app.py
+++ projeto faculdade Rogério/teste/app.py
@@ -1,4 +1,10 @@
-@app.route('/cadastro')
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/cadastro1')
 def cadastro():
-    return
-render_vscode('cadastro.html')
+    return render_template('cadastro1.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
